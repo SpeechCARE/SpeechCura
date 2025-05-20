1887251de3be9afe6f198ae439c90a38e9c856ad
sinarashidi <sinarashidi46@gmail.com>
2025-05-20 08:23:26 +0330

    Add Contrastive Augmentation

--- speechcura/non_generative/__init__.py
+++ speechcura/non_generative/__init__.py
@@ -1,3 +1,4 @@
 from .specaugment import SpecAugment
+from .contrastive_augmentation import ContrastiveAugmentation
 
-__all__ = ["SpecAugment"]
+__all__ = ["SpecAugment", "ContrastiveAugmentation"]
